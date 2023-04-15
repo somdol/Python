25c2ed17ab3f5028290276040b828e188af5fbe7
somdol <127670616+somdol@users.noreply.github.com>
2023-04-15 16:51:07 +0900

    hw6

---
+++ hw6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[34]:
+
+
+for i in range(1,10):
+    for j in range (2,6):
+        print(j,'x',i ,'=', i*j,end='\t')
+    print()
+print()
+for i in range(1,10):
+    for j in range(6,10):
+        print(j,'x',i ,'=', i*j,end='\t')
+    print()
+
+
+# In[ ]:
+
+
+
+
